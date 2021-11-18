576a0091b1ed71e7a12610c0525bc5db8c305264
roy2651 <roy2651@gmail.com>
2021-11-18 11:30:14 +0800

    first push

---
+++ pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<u-swiper :list="list1" @change="change" @click="click"></u-swiper>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			list1: ['https://cdn.uviewui.com/uview/swiper/swiper1.png', 'https://cdn.uviewui.com/uview/swiper/swiper2.png', 'https://cdn.uviewui.com/uview/swiper/swiper3.png'],
+			show: true
+		};
+	}
+};
+</script>
