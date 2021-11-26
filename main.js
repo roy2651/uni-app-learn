1419bc9b9eab27811ad3122c91de32858dd85e3c
roy2651 <roy2651@gmail.com>
2021-11-26 14:35:45 +0800

    api拦截器

--- main.js
+++ main.js
@@ -7,18 +7,31 @@ import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+	...App
 })
 Vue.use(uView);
+// 接口请求挂载
+import httpInterceptor from '@/common/http.interceptor.js'
+// 这里需要写在最后，是为了等Vue创建对象完成，引入"app"对象(也即页面的"this"实例)
+Vue.use(httpInterceptor, app)
+// http接口API集中管理引入部分
+import httpApi from '@/common/http.api.js'
+Vue.use(httpApi, app)
+
+// 自定义工具
+import utils from "@/common/utils.js"
+Vue.use(utils, app)
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,80 +1,100 @@
 <template>
-	<view class="content">
-		<u-row gutter="16" justify="space-between">
-			<u-col :span="11"><u-search placeholder="搜索物品" v-model="keyword" :show-action="false"></u-search></u-col>
-			<u-col :span="1"><u-icon name="scan" size="46"></u-icon></u-col>
-		</u-row>
-		<u-row gutter="16">
-			<u-col :span="6">
-				<view class="showCard">
-					<u-icon name="clock" color="#2979ff" size="44"></u-icon>
-					<text class="title">临期</text>
-					<u-count-to :start-val="0" :end-val="l_endVal" font-size="35"></u-count-to>
-				</view>
+	<view class="u-p-b-20">
+		<u-swiper :list="swperList" height="328" name="img_url"></u-swiper>
+		<!-- tabs -->
+		<view class="u-text-center u-m-t-20">
+			<u-tabs :gutter="20" :item-width="160" :bar-height="12" :list="sortList" :current="sortCurrent" @change="sortChange"></u-tabs>
+		</view>
+		<u-row gutter="16"  class="u-skeleton">
+			<u-col span="6" v-for="(goods,index) in goodsList.length ? goodsList : 4" :key="index">
+				<goods-card :goods="goods"></goods-card>
 			</u-col>
-			<u-col :span="6">
-				<view class="showCard">
-					<u-icon name="bell" color="#f29100" size="44"></u-icon>
-					<text class="title">过期</text>
-					<u-count-to :start-val="0" :end-val="g_endVal" font-size="35"></u-count-to>
-				</view>
+			<u-col span="12">
+				<u-loadmore :status="isShowLoading" icon-color="#2979ff" color="#2979ff" />
 			</u-col>
 		</u-row>
+		<!-- 返回顶部 -->
+		<u-back-top :scroll-top="scrollTop"></u-back-top>
+		<!-- 骨架屏 -->
+		<u-skeleton :loading="loading" :animation="true" bgColor="#fff"></u-skeleton>
 	</view>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			keyword: '',
-			l_endVal: 3,
-			g_endVal: 4,
-			list: [
-				{
-					text: '点赞',
-					color: 'blue',
-					fontSize: 28
-				},
-				{
-					text: '分享'
-				},
-				{
-					text: '评论'
+	export default {
+		async onLoad() {
+			this.getIndexData()
+			// const params={
+			// 	title:"一眼万年",
+			// 	// 	cover:"https://ts1.cn.mm.bing.net/th/id/R-C.7ffc7c3a12c64f7a218626d04782455c?rik=iXnx6mCcBGr%2bgg&riu=http%3a%2f%2fwx4.sinaimg.cn%2fmw690%2f6a04b428gy1ful02sqvwfg209q08swg6.gif&ehk=kJ0ydvYbzRh2GbDnshKLCnTF6RyIsKNh8okn4IQeAcc%3d&risl=&pid=ImgRaw&r=0"
+			// 	img:"https://ts2.cn.mm.bing.net/th/id/OIP-C.xtw-5wgzdbWqoEr9tgJIMgHaEo?w=285&h=180&c=7&r=0&o=5&pid=1.7",
+				
+			// }
+			// await this.$u.api.setBanner(81,params)
+			// // await this.$u.api.sortBanner(81,1)
+			// const res=await this.$u.api.getBanner()
+			// console.log(res)
+		},
+		data() {
+			return {
+				swperList: [],
+				goodsPage:1,
+				goodsList:[],
+				sortList: [
+					{name:"默认"},
+					{name:"销量"},
+					{name:"推荐"},
+					{name:"最新"},
+				],
+				sortCurrent: 0,
+				isShowLoading:'loading',
+				loading: false, // 是否显示骨架屏组件
+				scrollTop: 0,
+			}
+		},
+		methods: {
+			sortChange(index) {
+				this.sortCurrent = index;
+				// 重置商品数据和分页
+				this.page=1
+				this.goodsList=[]
+				this.getIndexData()
+			},
+			
+			async getIndexData(){
+				this.loading=!this.loading
+				const params = {
+					page:this.goodsPage
 				}
-			],
-			show: true
-		};
-	},
-	onLoad() {},
-	methods: {}
-};
+				// 判断tabs
+				if(this.sortCurrent===1) params.sales=1
+				if(this.sortCurrent===2) params.recommend=1
+				if(this.sortCurrent===3) params.new=1
+				// 拿到首页商品数据
+				const indexData = await this.$u.api.getIndex(params)
+				this.swperList=indexData.slides
+				// 将分页数据累加上
+				this.goodsList.push(...indexData.goods.data)
+				this.loading=!this.loading
+			}
+		},
+		onReachBottom() {
+			// 如果请求到的数据不再是10条，说明数据到底
+			if(this.goodsList.length % 10 ==0){
+				this.goodsPage++
+				this.getIndexData()
+			}else{
+				this.isShowLoading="nomore"
+			}
+		},
+		onPageScroll(e) {
+			// 返回顶部
+			this.scrollTop = e.scrollTop;
+		}
+	}
 </script>
 
-<style>
-.content {
-	display: flex;
-	flex-direction: column;
-	padding: 20rpx;
-}
-.u-row {
-	margin: 30rpx 0;
-}
-
-.showCard {
-	height: 120rpx;
-	border-radius: 4px;
-	transition: 0.5s;
-	color: #fff;
-	box-shadow: 0px 5px 25px rgba(65, 84, 241, 0.3);
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	padding: 20rpx;
-}
-.title {
-	color: #303133;
-	margin: 0 50rpx 0 5rpx;
-	font-size: larger;
-}
+<style lang="scss" scoped>
+	
+	
 </style>
